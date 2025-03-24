2fbc6e454cda76e914e0879c93a3026c06b8ff51
Jon Gunderson <jongund@illinois.edu>
2025-03-24 16:10:57 -0500

    communication between sidebar and content script

--- extension-firefox/content.js
+++ extension-firefox/content.js
@@ -1,3 +1,65 @@
 /* content.js */
 
+const debug = true;
+
 console.log(`[content.js]: loading`);
+
+const browserRuntime = typeof browser === 'object' ?
+              browser.runtime :
+              chrome.runtime;
+
+debug && console.log(`[content.js][browserRuntime]: ${browserRuntime}`);
+
+browserRuntime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "[content.js]: from a content script:" + sender.tab.url :
+                "[content.js]: from the extension");
+
+    console.log(`[content.js][request]: ${request.greeting}`);
+
+    const headingNodes = Array.from(document.querySelectorAll('h1, h2, h3, h4, h5, h6'));
+
+    const headingInfo = [];
+
+    headingNodes.forEach( (h) => {
+      console.log(`[heading][${h.tagName}]: ${h.textContent}`);
+
+      const tag = h.tagName;
+      const txt = h.textContent.trim();
+      headingInfo.push({
+        tagName: tag,
+        name: txt
+      });
+    });
+
+    const regionNodes = Array.from(
+      document.querySelectorAll(`
+        aside,
+        body > footer,
+        body > header,
+        form,
+        main,
+        nav,
+        search,
+        section[aria-label],
+        section[aria-labelledby]
+      `));
+
+    const regionInfo = [];
+
+    regionNodes.forEach( (r) => {
+      console.log(`[region][${r.tagName}]`);
+
+      const tag = r.tagName.toLowerCase();
+      regionInfo.push({
+        tagName: tag
+      });
+    });
+
+    if(request.greeting) {
+      sendResponse({headings: headingInfo, regions: regionInfo});
+    }
+    return true;
+  }
+);
--- src/sidepanel.js
+++ src/sidepanel.js
@@ -45,6 +45,88 @@ const browserTabs = typeof browser === 'object' ?
 
 debug && console.log(`[browserTabs]: ${browserTabs}`);
 
+let myWindowId = -1;  // used for checking if a tab is in the same window as the sidebar
+
+let ulHeadings;
+let ulRegions;
+
+
+// Initialize controls in Side Panel
+window.addEventListener('load', () => {
+
+  const btnGetInfo = document.querySelector("#id-btn-get-info");
+  ulHeadings = document.querySelector("#id-ul-headings");
+  ulRegions  = document.querySelector("#id-ul-regions");
+
+  debug && console.log(`[ulHeadings]: ${ulHeadings}`);
+  debug && console.log(`[ulRegions ]: ${ulRegions}`);
+
+  btnGetInfo.addEventListener('click', () => {
+    debug && console.log(`[Get Information Button][click]`);
+
+    myBrowser.tabs
+      .query({
+        currentWindow: true,
+        active: true,
+      })
+      .then(sendMessageToTabs)
+      .catch(onError);
+
+
+  });
+
+});
+
+function onError(error) {
+  console.error(`Error: ${error}`);
+}
+
+async function sendMessageToTabs(tabs) {
+  debug && console.log(`[sendMessageToTabs]`);
+  for (const tab of tabs) {
+    debug && console.log(`[sendMessageToTabs][tab]: ${tab.id}`);
+    const myResult = await myBrowser.tabs
+      .sendMessage(tab.id, { greeting: "Hi from side panel script" });
+
+     while(ulHeadings.firstChild) {
+      ulHeadings.removeChild(ulHeadings.firstChild);
+     }
+
+    if (myResult.headings) {
+      myResult.headings.forEach( (h) => {
+        const liNode = document.createElement('li');
+        liNode.textContent = `${h.tagName}: ${h.name}`;
+        ulHeadings.appendChild(liNode);
+        console.log(liNode.textContent);
+      });
+    }
+
+     while(ulRegions.firstChild) {
+      ulRegions.removeChild(ulRegions.firstChild);
+     }
+
+    if (myResult.regions) {
+      myResult.regions.forEach( (r) => {
+        const liNode = document.createElement('li');
+        liNode.textContent = `${r.tagName}`;
+        ulRegions.appendChild(liNode);
+        console.log(liNode.textContent);
+      });
+    }
+
+  }
+}
+
+//-----------------------------------------------
+//  Functions communicating with content script
+//-----------------------------------------------
+
+
+
+//-----------------------------------------------
+//  Functions that handle tab and window events
+//-----------------------------------------------
+
 /*
 *   Add event listeners when sidebar loads
 */
@@ -62,10 +144,6 @@ window.addEventListener('unload', function () {
 });
 
 
-//-----------------------------------------------
-//  Functions that handle tab and window events
-//-----------------------------------------------
-
 /*
 **  Handle tabs.onUpdated event when status is 'complete'
 */
@@ -82,6 +160,7 @@ function handleTabUpdated (tabId, changeInfo, tab) {
     }
     console.log(`[handleTabUpdated][status]: ${changeInfo.status}`);
   }
+
   if (changeInfo.status === "complete") {
     debug && console.log(`[handleTabUpdated][status]: complete`);
     lastStatus = changeInfo.status;
@@ -101,8 +180,11 @@ function handleTabUpdated (tabId, changeInfo, tab) {
 async function logTabUrl(info) {
   try {
     let tab = await browserTabs.get(info.tabId);
-    console.log(`[handleTabActivated][ id]: ${tab.id}`);
-    console.log(`[handleTabActivated][url]: ${tab.url}`);
+    console.log(`[handleTabActivated][ myWindowId]: ${myWindowId}`);
+    console.log(`[handleTabActivated][   windowId]: ${tab.windowId}`);
+    console.log(`[handleTabActivated][same window]: ${tab.windowId === myWindowId}`);
+    console.log(`[handleTabActivated][         id]: ${tab.id}`);
+    console.log(`[handleTabActivated][        url]: ${tab.url}`);
   }
   catch (error) {
     console.error(error);
@@ -123,7 +205,6 @@ function handleTabActivated (activeInfo) {
 **  Handle window focus change events: If the sidebar is open in the newly
 **  focused window, save the new window ID and update the sidebar content.
 */
-let myWindowId = '';
 function handleWindowFocusChanged (windowId) {
   debug && console.log(`[handleWindowFocusChanged][windowId]: ${windowId}`);
   if (windowId !== myWindowId) {
